a4dbcfcb629da4ad0363f97d97e832a1d093c2f5
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-05-17 13:15:38 -0300

    POO

---
+++ getter-setter/carro.js
@@ -0,0 +1,36 @@
+const speed = Symbol('velocidade');
+class Carro{
+    constructor(nome){
+        this.nome = nome
+        this[speed] = 77;
+
+    }
+
+    set velocidade(valor){
+        if(typeof valor !== 'number') return;
+        if(valor >= 100 || valor <= 0) return;
+        this[speed] = valor;
+    }
+
+    get velocidade(){
+        return this[speed];
+    }
+
+    acelerar(){
+        if(this[speed] >= 100) return;
+        this[speed]++;
+    }
+
+    freiar(){
+        if(this[speed] <= 0) return
+        this[speed]--
+    }
+}
+
+const c1 = new Carro('Civic')
+for(let i = 0; i <= 200; i++){
+    c1.acelerar();
+}
+c1.freiar();
+
+console.log(c1.velocidade); 
---
+++ getter-setter/pessoa.js
@@ -0,0 +1,22 @@
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    get NomeCompleto(){
+        return this.nome + ' ' + this.sobrenome;
+    }
+
+    set NomeCompleto(valor){
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome = valor.join(' ');
+    }
+}
+
+const p1 = new Pessoa('Vitor', 'Souza')
+p1.NomeCompleto = 'Vitor Souza Firmino'
+console.log(p1)
+console.log(p1.nome)
+console.log(p1.sobrenome)
